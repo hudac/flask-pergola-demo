678f36cd4e15630ba410aca70653b8d0391a261f
Hüdaverdi Cakir <hudaverdi.cakir@datasophie.de>
2025-12-04 16:38:51 +0000

    fix: alembic config parser was failing when pwd contained special / escaped chars

--- app/migrate.py
+++ app/migrate.py
@@ -5,5 +5,5 @@ from alembic.config import Config
 def run_sql_migrations(db, migrations_path: str) -> None:
     config = Config()
     config.set_main_option("script_location", migrations_path)
-    config.set_main_option("sqlalchemy.url", db.engine.url.render_as_string(hide_password=False))
+    config.set_main_option("sqlalchemy.url", db.engine.url.render_as_string(hide_password=False).replace("%", "%%"))
     upgrade(config, "head")
